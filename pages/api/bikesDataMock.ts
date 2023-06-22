956af10917f76b533e9328b732b40c2cc9362ac9
Francisco Tarantuviez <franciscotarantuviez@Franciscos-MBP.lan>
2023-06-21 21:28:18 -0300

    feat: generate mock data

--- pages/api/bikesDataMock.ts
+++ pages/api/bikesDataMock.ts
@@ -37,8 +37,84 @@ const vintageBikes: Bike[] = [
   },
 ];
 
-const normalBikes: Bike[] = [];
+const normalBikes: Bike[] = [
+  {
+    id: "5",
+    type: "normal",
+    image: "/images/bikes/normal/1.png",
+    description:
+      "With relaxed geometry, wide range Shimano gearing, and 40c tyres, moving by bike has never been so easy. The Transit frame is made from lightweight hydroformed alloy delivering a nimble and accurate ride, making you feel at home instantly. Cables are routed internally to give a sleek look and improve protection, with the fork crown and headtube size matched to improve looks and reduce dirt ingress to the headset.",
+    title: "GET MOVING",
+    color: "#1e73be",
+  },
+  {
+    id: "6",
+    type: "normal",
+    image: "/images/bikes/normal/2.png",
+    description:
+      "The Transit WSD frame is made from lightweight hydroformed alloy and has geometry designed with consideration of female frames, maximising comfort and performance. Cables are routed internally to give a sleek look and improve protection, with the fork crown and headtube size matched to improve looks and reduce dirt ingress to the headset.",
+    title: "TRANSIT WSD BIKE MY23",
+    color: "#cfe4fd",
+  },
+  {
+    id: "7",
+    type: "normal",
+    image: "/images/bikes/normal/3.png",
+    description:
+      "Luggage and mudguards are easily fitted using the frames mounts making customization to your needs easy. And with a sturdy chain stay mounted kickstand loading and unloading is always easy. Pro disc models are equipped with Nutt hydraulic discs and upgraded wheelsets to really enhance the ride quality and control.",
+    title: "TRANSIT PRO DISC BIKE MY23",
+    color: "#919191",
+  },
+  {
+    id: "8",
+    type: "normal",
+    image: "/images/bikes/normal/4.png",
+    description:
+      "Luggage and mudguards are easily fitted using the frames mounts making customization to your needs easy. And with a sturdy chain stay mounted kickstand loading and unloading is always easy. Pro disc models are equipped with Nutt hydraulic discs and upgraded wheelsets to really enhance the ride quality and control.",
+    title: "TRANSIT PRO DISC WSD BIKE MY23",
+    color: "#c0cac6",
+  },
+];
 
-const bikes: Bike[] = [...vintageBikes, ...normalBikes];
+const electricBikes: Bike[] = [
+  {
+    id: "9",
+    type: "electric",
+    image: "/images/bikes/electric/1.png",
+    description:
+      "With relaxed geometry, wide range Shimano gearing, and 40c tyres, moving by bike has never been so easy. The Transit frame is made from lightweight hydroformed alloy delivering a nimble and accurate ride, making you feel at home instantly. Cables are routed internally to give a sleek look and improve protection, with the fork crown and headtube size matched to improve looks and reduce dirt ingress to the headset.",
+    title: "QUEST 1.0 EBIKE MY23",
+    color: "#2a2628",
+  },
+  {
+    id: "10",
+    type: "electric",
+    image: "/images/bikes/electric/2.png",
+    description:
+      "With relaxed geometry, wide range Shimano gearing, and 40c tyres, moving by bike has never been so easy. The Transit frame is made from lightweight hydroformed alloy delivering a nimble and accurate ride, making you feel at home instantly. Cables are routed internally to give a sleek look and improve protection, with the fork crown and headtube size matched to improve looks and reduce dirt ingress to the headset.",
+    title: "QUEST 2.0 EBIKE MY23",
+    color: "#c0cac6",
+  },
+  {
+    id: "11",
+    type: "electric",
+    image: "/images/bikes/electric/3.png",
+    description:
+      "With relaxed geometry, wide range Shimano gearing, and 40c tyres, moving by bike has never been so easy. The Transit frame is made from lightweight hydroformed alloy delivering a nimble and accurate ride, making you feel at home instantly. Cables are routed internally to give a sleek look and improve protection, with the fork crown and headtube size matched to improve looks and reduce dirt ingress to the headset.",
+    title: "Blacktop 2.0 eBike MY23",
+    color: "#697e91",
+  },
+  {
+    id: "12",
+    type: "electric",
+    image: "/images/bikes/electric/4.png",
+    description:
+      "With relaxed geometry, wide range Shimano gearing, and 40c tyres, moving by bike has never been so easy. The Transit frame is made from lightweight hydroformed alloy delivering a nimble and accurate ride, making you feel at home instantly. Cables are routed internally to give a sleek look and improve protection, with the fork crown and headtube size matched to improve looks and reduce dirt ingress to the headset.",
+    title: "Blacktop 2.0 Step Through eBike MY23",
+    color: "#697e91",
+  },
+];
+
+const bikes: Bike[] = [...vintageBikes, ...normalBikes, ...electricBikes];
 
 export default bikes;
